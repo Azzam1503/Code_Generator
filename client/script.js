9bca1f90ab7e06476190cfdffe6b76e140253b02
Azzam Uddin <uazzam69@gmail.com>
2023-01-13 21:32:41 +0530

    Completed Frontend

--- client/script.js
+++ client/script.js
@@ -1,8 +1,8 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_Container');
+const form = document.querySelector('#form');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInerval;
 
@@ -30,3 +30,59 @@ function typeText(element, text){
     }
   }, 20)
 }
+
+function generateUinqueId(){
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexadecimalString}`;
+}
+
+function chatStripe (isAi, value, uniqueId){
+  return (
+    `
+    <div class='wrapper' ${isAi && 'ai'}>
+      <div class='chat'>
+        <div class='profile'>
+          <img src="${isAi ? bot : user}"
+          alt="${isAi ? 'bot' : 'user'}"
+          />
+        </div>
+        <div class="message" id=${uniqueId}>${value}></div>
+      </div>
+    </div>
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+ 
+  //user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  
+  form.reset();
+  
+  //bot's chatstripe
+  const uniqueId = generateUinqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13){
+    handleSubmit(e)
+  }
+})
+
+
